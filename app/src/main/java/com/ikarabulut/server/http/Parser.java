f29d8377d23e4a7a5c3eeede30c59ba539f57fda
Ismail Karabulut <i.karabulut93@gmail.com>
2022-07-25 00:18:53 -0500

    adds input parsing functionality

---
+++ app/src/main/java/com/ikarabulut/server/http/Parser.java
@@ -0,0 +1,88 @@
+package com.ikarabulut.server.http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+    InputStream connectionInputStream;
+    Map<String, String> requestInitialLine = new HashMap<>();
+    Map<String, String> requestHeaders = new HashMap<>();
+    String requestBody;
+
+    public Parser(InputStream connectionInputStream) {
+        this.connectionInputStream = connectionInputStream;
+    }
+
+    public void parse() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(this.connectionInputStream));
+        StringBuilder builder = new StringBuilder();
+        String[] splitRequest = generateSplitRequest(reader, builder);
+
+        String initialLineAndHeaders = splitRequest[0];
+        String[] splitInitialLineAndHeader = initialLineAndHeaders.split("\r\n");
+        String initialLine = splitInitialLineAndHeader[0];
+
+        String[] initialLineParts = initialLine.split(" ", 3);
+        this.requestInitialLine = parseInitialLine(initialLineParts);
+
+        this.requestHeaders = parseHeaders(splitInitialLineAndHeader);
+
+        this.requestBody = null;
+        if (splitRequest.length > 1) {
+            this.requestBody = splitRequest[1];
+        }
+
+    }
+
+    public Map<String, String> getRequestInitialLine() {
+        return this.requestInitialLine;
+    }
+
+    public Map<String, String> getRequestHeaders() {
+        return this.requestHeaders;
+    }
+
+    public String getRequestBody() {
+        return this.requestBody;
+    }
+
+    private String[] generateSplitRequest(BufferedReader reader, StringBuilder builder) throws IOException {
+        int availableBytes = connectionInputStream.available();
+        int character;
+        int bytesRead = 0;
+        while (bytesRead < availableBytes) {
+            character = reader.read();
+            if (!reader.ready()) {
+                builder.append((char) character);
+                break;
+            }
+            builder.append((char) character);
+            bytesRead++;
+        }
+        return builder.toString().split("\r\n\r\n");
+    }
+
+    private Map<String, String> parseInitialLine(String[] initialLineParts) {
+        requestInitialLine.put("method", initialLineParts[0]);
+        requestInitialLine.put("resource", initialLineParts[1]);
+        requestInitialLine.put("version", initialLineParts[2]);
+        return requestInitialLine;
+    }
+
+    private Map<String, String> parseHeaders(String[] splitInitialLineAndHeader) {
+        for (int i = 1; i < splitInitialLineAndHeader.length; i++) {
+            if (!splitInitialLineAndHeader.equals("")) {
+                String[] line = splitInitialLineAndHeader[i].split(":", 2);
+                String headerName = line[0];
+                String headerValue = line[1];
+                this.requestHeaders.put(headerName, headerValue);
+            }
+        }
+        return requestHeaders;
+    }
+
+}
---
+++ app/src/test/java/com/ikarabulut/server/http/ParserTest.java
@@ -0,0 +1,84 @@
+package com.ikarabulut.server.http;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    private final String input = "HEAD /test HTTP/1.1\r\n" +
+            "Content-Type" + ":" + "text/plain\r\n" +
+            "User-Agent" + ":" + "PostmanRuntime/7.29.0\r\n\r\n" +
+            "Hello World";
+
+    private final String inputWithoutBody = "HEAD /test HTTP/1.1 \r\n" +
+            "Content-Type" + ":" + "text/plain\r\n" +
+            "User-Agent" + ":" + "PostmanRuntime/7.29.0\r\n\r\n";
+
+    @Test
+    void parse_ShouldSetInputLine_ToEqualInputStreamsInitialLine() throws IOException {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Parser parser = new Parser(in);
+        Map<String, String> expectedInitialLine = new HashMap<>();
+        expectedInitialLine.put("method", "HEAD");
+        expectedInitialLine.put("resource", "/test");
+        expectedInitialLine.put("version", "HTTP/1.1");
+
+        parser.parse();
+        Map<String, String> parsedInitialLine = parser.getRequestInitialLine();
+
+        assertEquals(expectedInitialLine, parsedInitialLine);
+
+        in.close();
+    }
+
+    @Test
+    void parse_ShouldSetHeaders_ToEqualInputStreamsHeaders() throws IOException {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Parser parser = new Parser(in);
+        Map<String, String> expectedHeaders = new HashMap<>();
+        expectedHeaders.put("Content-Type", "text/plain");
+        expectedHeaders.put("User-Agent", "PostmanRuntime/7.29.0");
+
+        parser.parse();
+        Map<String, String> parsedHeaders = parser.getRequestHeaders();
+
+        assertEquals(expectedHeaders, parsedHeaders);
+
+        in.close();
+    }
+
+    @Test
+    void parse_ShouldSetBody_ToEqualInputStreamsBody_IfBodyExists() throws IOException {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Parser parser = new Parser(in);
+        String expectedParsedBody = "Hello World";
+
+        parser.parse();
+        String parsedBody = parser.getRequestBody();
+
+        assertEquals(expectedParsedBody, parsedBody);
+
+        in.close();
+    }
+
+    @Test
+    void parse_ShouldSetBody_ToNull_IfNoBodyExists() throws IOException {
+        InputStream in = new ByteArrayInputStream(inputWithoutBody.getBytes());
+        Parser parser = new Parser(in);
+
+        parser.parse();
+        String parsedBody = parser.getRequestBody();
+
+        assertNull(parsedBody);
+
+        in.close();
+    }
+
+}
